8907dd9a47f109deb4f542d4c54ad68780f441b7
serayk <seraykaraefe@gmail.com>
2022-08-02 10:58:39 +0300

    no message

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func additionClicked(_ sender: Any) {
+        if let firstRes = Int(firstNumber.text!){
+   
 
+            if let secondRes = Int(secondNumber.text!){
+                
+
+                let answer = firstRes + secondRes
+                
+
+                result.text = String(answer)
+            }
+        }
+
+    }
+        
+    @IBAction func subtractionClicked(_ sender: Any) {
+        if let firstRes = Int(firstNumber.text!){
+
+
+            if let secondRes = Int(secondNumber.text!){
+         
+
+                let answer = firstRes - secondRes
+  
+
+                result.text = String(answer)
+            }
+        }
+        
+    }
+    @IBAction func divisionClicked(_ sender: Any) {
+        if let firstRes = Int(firstNumber.text!){
+         
+
+            if let secondRes = Int(secondNumber.text!){
+            
+
+                let answer = firstRes / secondRes
+      
+
+                result.text = String(answer)
+            }
+        }
+        
+    }
+    @IBAction func multiplicationClicked(_ sender: Any) {
+        if let firstRes = Int(firstNumber.text!){
+           
+
+            if let secondRes = Int(secondNumber.text!){
+              
+
+                let answer = firstRes * secondRes
+
+
+                result.text = String(answer)
+            }
+        }
+        
+    }
 }
 
